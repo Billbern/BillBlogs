1219ac9b44a2b65419d1f4162eba9496733ac4ea
Billbern <denis.abaidoo@gmail.com>
2020-11-14 19:08:31 +0000

    added admin route and start script

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 
 
+app.get('/admin', (req, res)=>{
+    res.send("Admin page");
+})
 
 
 app.get('/', (req, res)=>{
     res.send("Hello Blog");
 })
-
 
 
 app.listen(5060, ()=>{
